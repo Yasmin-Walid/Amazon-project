fef3446ce207f5a5f4d287292fd4cd82296f5283
Yasmin-Walid <yasmin.elsnossy@gmail.com>
2024-10-21 22:27:21 +0300

    save the cart into local storage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,27 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2  
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
-  }
-];
+export let cart = 
+
+JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart = 
+  [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2  
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1
+    }
+  ];
+}
+
+function saveToStorage (){
+
+  localStorage.setItem('cart',JSON.stringify(cart))
+
+}
+
 
 export function addToCart (productId){
     let matchingItem; /* This variable is declared but not initialized (i.e., no value is assigned at the time of declaration).*/
@@ -24,6 +38,7 @@ export function addToCart (productId){
        quantity: 1
      });
    }
+   saveToStorage ();
   };
 
   export function removeFromCart(productID){
@@ -36,4 +51,5 @@ export function addToCart (productId){
     });
 
     cart = newCart /* reassigning the value */
+    saveToStorage ();
   };
