4ddc73581c1bfac463161dc44a2caa9b99328808
Yasmin-Walid <yasmin.elsnossy@gmail.com>
2024-10-24 01:25:34 +0300

    save Data and generate HTML for deliveryOptions

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,44 +2,6 @@ import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
-/* const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating:{
-      stars: 4.5,
-      count:87
-    },
-      priceCents: 1090
-    
-  }, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball' ,
-    rating: {
-      stars: 4,
-      count: 127
-    } ,
-    priceCents: 2095
-  },{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating:{
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799
-  }, {
-    image: 'images/products/black-2-slot-toaster.jpg',
-    name: '2 Slot Toaster - Black',
-    rating: {
-      stars: 5,
-      count: 2197
-    },
-    priceCents: 1899
-  }
-
-  
-  ]; */
-
 let productsHTML = ''; /* By initializing productsHTML as an empty string, you set up a base to which you can append HTML content later in the code. It acts as a container that will eventually hold the complete HTML structure for all the products. */
 
 /* the role of the product parameter:  For each iteration, the current product object is passed to the callback function as the product parameter. This allows you to access the properties of the current product, such as image, name, rating, and priceCents. */
